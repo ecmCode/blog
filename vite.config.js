cd44cf940e7810ad8bf533b50293c12da05b461c
Lee <elcheveremochilero@gmail.com>
2022-11-18 17:19:55 -0600

    fix bugs

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base:'/',
-  base:'/blog/',
+  base:'/',
   plugins: [react()]
 })
+
+// npm run build
+// git subtree push --prefix dist origin gh-pages
