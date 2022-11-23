1695d896753c781bd698570f2c26b1b5bfefec25
Lee <elcheveremochilero@gmail.com>
2022-11-22 18:03:17 -0600

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
             bg-rose-500 text-4xl bg-opacity-20 select-none text-rose-200">
             <h2>Blogger</h2>
             <ul className="flex gap-16 text-lg font-semibold"> 
-                <Link to='/'>Home</Link>
+                <Link to='/home'>Home</Link>
                 <Link to='/blog'>Blog</Link>
                 <Link to='/contact'>Contact</Link>
             </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Header />
-      
         <Routes>
-          <Route path='/' exact element={<Home/>} />
-          <Route path='/blog' element={<Blog/>} />
-          <Route path='/contact' element={<Contact/>} />
-          <Route path='/404' element={<NotFound/>} />
+          <Route path='/home' exact element={<Home/>} />
+          <Route path='/blog' exact element={<Blog/>} />
+          <Route path='/contact' exact element={<Contact/>} />
+          <Route path='/404' exact element={<NotFound/>} />
+          <Route path="/blogger" element={<Navigate to="/home" />} />
           <Route path="*" element={<Navigate to="/404" />} />
         </Routes>
     </BrowserRouter>    
